3295ea84d3c2d677585cd8764feebac852c58a8b
JackRoss <atrinifco2004@gmail.com>
2024-06-05 15:04:52 -0600

    Modificacion de los colores del home y eliminacion del archivo de prueba

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,20 +1,23 @@
-import React from 'react'
 import Input from '../components/Input'
 import { styles } from '../assets/styles/global-styles'
+import { hexToRgba } from '../assets/styles/hexToRgba';
 
 const Home = () => {
-  const {backegrounGreen} = styles;
+  const { backegrounGreen } = styles;
+  const backgroundColorWithTransparency = hexToRgba(styles.backgroundHome, 0.0); // 0.5 es la transparencia deseada
   return (
     <div className='flex justify-center items-center h-full mt-20'>
-      <div className="border-gray-200 border-[1px] p-10 shadow-sm">
+      <div className="border-green-600 border-[5px] p-10 shadow-sm rounded-3xl" style={{ backgroundColor: backgroundColorWithTransparency }}>
         <div className="">
-          <p className='text-xl font-bold'>Iniciar sesión</p>
-          <p className='text-gray-400 font-semibold'>Ingresa tu usuario y contraseña para iniciar sesión.</p>
+          <p className='text-xl font-bold text-black'>Iniciar sesión</p>
+          <p className='font-semibold text-black'>Ingresa tu usuario y contraseña para iniciar sesión.</p>
         </div>
-        <div className="flex flex-col gap-5 my-10">
-          <Input name={"Usuario"} type={"text"} placeholder={"Ingresa tu usuario"} />
-          <Input name={"Contraseña"} type={"password"} placeholder={"Ingresa tu contraseña"} />
-          <button style={{backgroundColor:backegrounGreen}} className='text-white p-2 rounded-sm font-semibold'>Iniciar Sesión</button>
+        <div className="flex flex-col gap-5 my-10 paddin">
+          <p className='text-black'>Usuario:</p>
+          <Input type={"text"} placeholder={"Ingresa tu usuario"} />
+          <p className='text-black'>Contraseña:</p>
+          <Input type={"password"} placeholder={"Ingresa tu contraseña"} className="text-black" />
+          <button style={{ backgroundColor: backegrounGreen }} className='text-white p-2 rounded-sm font-semibold'>Iniciar Sesión</button>
         </div>
       </div>
     </div>
