3dfb9652b0e949a379a59f6e410f7d879ba30b76
JackRoss <atrinifco2004@gmail.com>
2024-06-05 15:18:56 -0600

    Modificando los colores de los inputs

--- frontend/src/components/Input.jsx
+++ frontend/src/components/Input.jsx
@@ -1,11 +1,10 @@
-import React from 'react'
 
 const Input = ({name,type,placeholder}) => {
   return (
     <div className='flex flex-col'>
         <label>{name}</label>
         <input 
-            className='border-gray-400 border-[1px] rounded-md p-2 focus:outline-none'
+            className='border-gray-300 border-[2px] rounded-md p-2 focus:outline-none'
             type={type} 
             placeholder={placeholder} 
         />
