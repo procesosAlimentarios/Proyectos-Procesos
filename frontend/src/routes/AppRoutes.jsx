4e413f78ccf542acafd28e05b72bf7a51c102047
JackRoss <atrinifco2004@gmail.com>
2024-06-06 14:24:29 -0600

    Agregando nuevas rutas

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,8 +1,13 @@
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import Home from '../pages/Home'
+
+import MaterialAditivos from '../pages/admin/MaterialAditivos'
 import AgregarNuevoAditivo from '../pages/admin/AgregarNuevoAditivo'
 import EditarAditivo from '../pages/admin/EditarAditivo'
-import Prueba from '../pages/Prueba'
+
+import AgregarNuevoAlimento from '../pages/admin/AgregarNuevoAlimento'
+import EditarAlimento from '../pages/admin/EditarAlimento'
+
 import NotFound from '../pages/NotFound'
 import Header from '../components/Header'
 
@@ -13,9 +18,14 @@ const AppRoutes = () => {
             <Routes>
                 <Route path='/' element={<Home />} />
                 <Route path='*' element={<NotFound />} />
-                <Route path='/Prueba' element={<Prueba/>}/>
-                <Route path='/AgregarNuevoAditivo' element={<AgregarNuevoAditivo/>}/>  
+
+                <Route path='/MaterialAditivo' element={<MaterialAditivos/>}/>
+                <Route path='/AgregarAditivo' element={<AgregarNuevoAditivo/>}/>  
                 <Route path='/EditarAditivo' element={<EditarAditivo/>}/>
+
+                <Route path='/AgregarAlimento' element={<AgregarNuevoAlimento/>}/>
+                <Route path='/EditarAlimento' element={<EditarAlimento/>}/>
+
             </Routes>
         </Router>
     )
